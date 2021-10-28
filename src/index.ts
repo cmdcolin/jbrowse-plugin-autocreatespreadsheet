fb44c2ecc4e6d6b6ee81614f86a4c688eebf8193
Colin <colin.diesh@gmail.com>
2021-10-28 18:29:46 -0400

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import Plugin from '@jbrowse/core/Plugin'
 import PluginManager from '@jbrowse/core/PluginManager'
 import ViewType from '@jbrowse/core/pluggableElementTypes/ViewType'
-import { AbstractSessionModel, isAbstractMenuManager } from '@jbrowse/core/util'
 import { version } from '../package.json'
 import {
   ReactComponent as HelloViewReactComponent,
@@ -23,13 +22,42 @@ export default class MyProjectPlugin extends Plugin {
   }
 
   configure(pluginManager: PluginManager) {
-    if (isAbstractMenuManager(pluginManager.rootModel)) {
-      pluginManager.rootModel.appendToSubMenu(['File', 'Add'], {
-        label: 'Open Hello!',
-        onClick: (session: AbstractSessionModel) => {
-          session.addView('HelloView', {})
-        },
-      })
-    }
+    //@ts-ignore
+    ;(async () => {
+      try {
+        pluginManager.rootModel?.setDefaultSession?.()
+
+        // add view, make typescript happy with return type
+        const view = pluginManager.rootModel?.session?.addView(
+          'SpreadsheetView',
+          {},
+        ) as
+          | {
+              importWizard: {
+                setFileType: Function
+                setSelectedAssemblyName: Function
+                setFileSource: Function
+                import: Function
+              }
+            }
+          | undefined
+
+        if (!view) {
+          throw new Error('Failed to initialize view')
+        }
+
+        // manually run through the import wizard steps
+        view.importWizard.setFileType('BED')
+        view.importWizard.setSelectedAssemblyName('hg19')
+        view.importWizard.setFileSource({
+          uri:
+            'https://s3.amazonaws.com/jbrowse.org/genomes/hg19/skbr3/reads_lr_skbr3.fa_ngmlr-0.2.3_mapped.bam.sniffles1kb_auto_l8_s5_noalt.new.vcf',
+          locationType: 'UriLocation',
+        })
+        view.importWizard.import('hg19')
+      } catch (e) {
+        console.error(e)
+      }
+    })()
   }
 }
